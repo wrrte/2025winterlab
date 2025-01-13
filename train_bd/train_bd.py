7d5d48f6d44f4a7bb811891bfa60214f20935ccc
choemj <choemj@kaist.ac.kr>
2025-01-13 16:37:17 +0900

    train setting 수정

--- train_bd/train_bd.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append('../ultralytics')
-
-from ultralytics.models import YOLOv10
-
-model = YOLOv10('weights/bd_01131614.pt')
-
-model.train(data='yolov10.yaml', epochs=500, batch=256, imgsz=640)
