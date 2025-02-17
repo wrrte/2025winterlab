2e56d5c542023f421bcc7cfd6050df0282eca80f
choemj <choemj@kaist.ac.kr>
2025-02-17 16:13:36 +0900

    merge

--- type_30000/train_bd_2.py
+++ type_30000/train_bd_2.py
@@ -6,6 +6,6 @@ os.environ["CUDA_VISIBLE_DEVICES"]="2" # 0, 1
 #nvitop -m compact --only-compute
 #duf --style ascii
 
-model = YOLO(model='/mnt/hdd_4A/choemj/2025winterlab/type_origin/weights/bd_trained/train_s/weights/best.pt')
+model = YOLO(model='/mnt/hdd_4A/choemj/2025winterlab/type_origin/weights/bd_trained/train_n/weights/best.pt')
 
-model.train(data='yolov11.yaml', epochs=2000, batch=64, imgsz=640, project="weight")
+model.train(data='yolov11.yaml', epochs=2000, batch=128, imgsz=640, project="weight")
--- type_30000/train_bd_3.py
+++ type_30000/train_bd_3.py
@@ -6,6 +6,6 @@ os.environ["CUDA_VISIBLE_DEVICES"]="3" # 0, 1
 #nvitop -m compact --only-compute
 #duf --style ascii
 
-model = YOLO(model='best.pt')
+model = YOLO(model='/mnt/hdd_4A/choemj/2025winterlab/type_origin/weights/bd_pretrained/yolo11m.pt')
 
-model.train(data='yolov11_3.yaml', epochs=2000, batch=32, imgsz=640, project='/mnt/hdd_4A/choemj/2025winterlab/type_30000/weight')
+model.train(data='yolov11.yaml', epochs=2000, batch=32, imgsz=640, project='/mnt/hdd_4A/choemj/2025winterlab/type_30000/weight')
