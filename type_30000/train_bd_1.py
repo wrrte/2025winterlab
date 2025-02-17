b747c6cca7f3109df66f0473828f7ea569a6aa62
choemj <choemj@kaist.ac.kr>
2025-02-17 15:08:10 +0900

    .

--- type_30000/train_bd_1.py
+++ type_30000/train_bd_1.py
@@ -6,6 +6,6 @@ os.environ["CUDA_VISIBLE_DEVICES"]="1" # 0, 1
 #nvitop -m compact --only-compute
 #duf --style ascii
 
-model = YOLO(model='/mnt/hdd_4A/choemj/2025winterlab/type_origin/weights/bd_trained/train_l/weights/best.pt')
+model = YOLO(model='/mnt/hdd_4A/choemj/2025winterlab/type_origin/weights/bd_pretrained/yolo11n.pt')
 
-model.train(data='yolov11.yaml', epochs=2000, batch=32, imgsz=640, project="weight")
+model.train(data='yolov11.yaml', epochs=2000, batch=128, imgsz=640, project="weight")
